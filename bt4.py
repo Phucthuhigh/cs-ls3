18db0e4f7d766ac2c8212b098ec4804d613564cf
Phucthuhigh <phucgamingpro@gmail.com>
2021-10-25 14:09:38 +0700

    second commit

--- bt4.py
+++ bt4.py
@@ -1,6 +1,6 @@
 try:
     n = int(input("Press n: "))
-    condition = [3,5]
+    condition = [3,5,2,1,6]
     a = []
 
     for i in condition:
@@ -15,8 +15,6 @@ try:
             s += f" {condition[i]} "
             if i != len(condition) - 1:
                 s += "or"
-    elif len(a) == 1:
-        s = f"{n} is divisible by {a[0]}"
     else:
         s = f"{n} is divisible by"
         for i in range(len(a)):
